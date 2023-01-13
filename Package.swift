8262e8e978ef9ac5e79959d70b8686038b812df2
Dilshan Edirisuriya <>
2023-01-13 15:18:36 +0000

    Updating package swift

--- Package.swift
+++ Package.swift
@@ -1,34 +1,26 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
-let dependencies: [PackageDescription.Package.Dependency] = [
-  .package(url: "https://github.com/SwiftyTesseract/SwiftyTesseract.git", .upToNextMajor(from: "4.0.1")),
-  .package(url: "https://github.com/alinradut/QKMRZParser.git", .upToNextMajor(from: "1.0.0")),
-]
-
 let package = Package(
-    name: "QKMRZScanner",
-    platforms: [.iOS(.v11), .macOS(.v10_13)],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "QKMRZScanner",
-            targets: ["QKMRZScanner"]),
-    ],
-    dependencies: dependencies,
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "QKMRZScanner",
-            dependencies: [
-                .product(name: "SwiftyTesseract", package: "SwiftyTesseract"),
-                .product(name: "QKMRZParser", package: "QKMRZParser"),
-            ],
-            path: "QKMRZScanner",
-            exclude: ["Supporting Files/Info.plist"],
-            resources: [.copy("Supporting Files/tessdata/ocrb.traineddata")]),
-    ]
+  name: "QKMRZScanner",
+  products: [
+    .library(
+      name: "QKMRZScanner",
+      targets: ["QKMRZScanner"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/itopstack/QKMRZParser", exact: "1.0.4"),
+    .package(url: "https://github.com/SwiftyTesseract/SwiftyTesseract", from: "4.0.1"),
+  ],
+  targets: [
+    .target(
+      name: "QKMRZScanner",
+      dependencies: ["QKMRZParser", "SwiftyTesseract"],
+      path: "QKMRZScanner"),
+    .testTarget(
+      name: "QKMRZScannerTests",
+      dependencies: ["QKMRZScanner"],
+      path: "Tests"),
+  ]
 )
