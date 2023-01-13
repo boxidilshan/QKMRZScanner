ff84418d26a4a74af74f5a954c9533bf3c643fe1
Dilshan Edirisuriya <>
2023-01-13 15:23:06 +0000

    update Swift package

--- Package.swift
+++ Package.swift
@@ -1,26 +1,27 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
+
 let package = Package(
-  name: "QKMRZScanner",
-  products: [
-    .library(
-      name: "QKMRZScanner",
-      targets: ["QKMRZScanner"]),
-  ],
-  dependencies: [
-    .package(url: "https://github.com/itopstack/QKMRZParser", exact: "1.0.4"),
-    .package(url: "https://github.com/SwiftyTesseract/SwiftyTesseract", from: "4.0.1"),
-  ],
-  targets: [
-    .target(
-      name: "QKMRZScanner",
-      dependencies: ["QKMRZParser", "SwiftyTesseract"],
-      path: "QKMRZScanner"),
-    .testTarget(
-      name: "QKMRZScannerTests",
-      dependencies: ["QKMRZScanner"],
-      path: "Tests"),
-  ]
+    name: "QKMRZScanner",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(name: "QKMRZScanner", targets: ["QKMRZScanner"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/jlab13/QKMRZParser.git", from: "1.0.3"),
+        .package(url: "https://github.com/SwiftyTesseract/SwiftyTesseract.git", from: "4.0.1"),
+    ],    
+    targets: [
+        .target(name: "QKMRZScanner",
+            dependencies: ["QKMRZParser", "SwiftyTesseract"],
+            path: "QKMRZScanner",
+                resources: [.copy("tessdata")]
+        ),
+    ],
+    swiftLanguageVersions: [.version("5")]
 )
