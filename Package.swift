01c284abc5fe22d2afb117c97443e68f0f1c3d42
Dilshan Edirisuriya <>
2023-01-15 16:51:07 +0000

    Updating configurations

--- Package.swift
+++ Package.swift
@@ -1,27 +1,33 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
+let dependencies: [PackageDescription.Package.Dependency] = [
+  .package(url: "https://github.com/SwiftyTesseract/SwiftyTesseract.git", .upToNextMajor(from: "4.0.0")),
+  .package(url: "https://github.com/alinradut/QKMRZParser.git", .upToNextMajor(from: "1.0.0")),
+]
 
 let package = Package(
     name: "QKMRZScanner",
-    platforms: [
-        .iOS(.v11)
-    ],
+    platforms: [.iOS(.v11), .macOS(.v10_13)],
     products: [
-        .library(name: "QKMRZScanner", targets: ["QKMRZScanner"]),
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "QKMRZScanner",
+            targets: ["QKMRZScanner"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/jlab13/QKMRZParser.git", from: "1.0.3"),
-        .package(url: "https://github.com/SwiftyTesseract/SwiftyTesseract.git", from: "4.0.0"),
-    ],    
+    dependencies: dependencies,
     targets: [
-        .target(name: "QKMRZScanner",
-            dependencies: ["QKMRZParser", "SwiftyTesseract"],
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "QKMRZScanner",
+            dependencies: [
+                .product(name: "SwiftyTesseract", package: "SwiftyTesseract"),
+                .product(name: "QKMRZParser", package: "QKMRZParser"),
+            ],
             path: "QKMRZScanner",
-                resources: [.copy("tessdata")]
-        ),
-    ],
-    swiftLanguageVersions: [.version("5")]
+            exclude: ["Supporting Files/Info.plist"],
+            resources: [.copy("Supporting Files/tessdata/ocrb.traineddata")]),
+    ]
 )
